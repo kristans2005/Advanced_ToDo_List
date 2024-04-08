37810666f18ce16332083fb2d8edd2ed829ed372
kristians2005 <mikelsons3333@gmail.com>
2024-04-08 23:15:59 +0300

    fetch utils added idk if works yet

--- frontend/src/utils/FetchingData.jsx
+++ frontend/src/utils/FetchingData.jsx
@@ -1,2 +1,21 @@
+import axios from 'axios';
 
 
+//Neesmu vēl iztestējis vai strādā
+export async function SendData(BCUrl, input){
+    try {
+        const response = await axios.post(BCUrl, input);
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error submitting form:', error);
+    }
+}
+
+export async function GetData(BCUrl, input){
+    try {
+        const response = await axios.get(BCUrl, input);
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error submitting form:', error);
+    }
+}
