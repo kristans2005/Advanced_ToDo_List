ed866a68b6a3500eea732a18b754f31cb49b8bb8
KandzaP <kandzapure@gmail.com>
2024-04-10 08:56:44 +0300

    db.sql

--- db.sql
+++ db.sql
@@ -4,5 +4,6 @@ USE advanced_todo;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
 );
