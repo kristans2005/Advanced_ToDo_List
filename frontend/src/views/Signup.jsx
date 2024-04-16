462af4941440b05404912165acf409803a5ee7f7
KandzaP <kandzapure@gmail.com>
2024-04-16 13:35:16 +0300

    afas

--- frontend/src/views/Signup.jsx
+++ frontend/src/views/Signup.jsx
@@ -74,6 +74,7 @@ function Signup() {
                     <label>
                         Username:
                         <input
+                        className='appearance-none border border-gray-300 rounded-md py-2 px-4 mb-3 leading-tight focus:outline-none focus:border-gray-500 focus:ring-gray-500'
                             type="text"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
