33d2f2eae703d8b7eeaa4364d4444d7ae2d13954
rudoxe <ipa22.r.vikmanis@vtdt.edu.lv>
2024-04-17 12:59:23 +0300

    erer

--- backend/models/todoModel.php
+++ backend/models/todoModel.php
@@ -20,11 +20,12 @@ function getTodoTable($userId) {
 }
 
 function getTodoBoard($TodoTableId) {
-    $sql = "SELECT * FROM todo_bord WHERE todo_table_id = :todo_table_id";
+    $sql = "SELECT * FROM todo_bord WHERE todo_table_id = :todo_table_id ORDER BY bord_column_order ASC";
     $stmt = $this->dbConnection->connection()->prepare($sql);
     $stmt->bindParam(':todo_table_id', $TodoTableId);
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 
 }
